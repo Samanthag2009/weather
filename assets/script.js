d27b869f98a2aad922049fcad96a1e1d3b50d958
Samantha Gosselin <samanthag2009@gmail.com>
2023-01-23 21:55:37 -0600

    added pseudo code, HMTML, visual elements

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+//insert weather API Key
+
+//weather API Key
+var APIKey = "4af337e04f29675ecae50ddb8a8e1565";
+
+//collect user input for city name 
+
+var city;
+
+//query URL for API call
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+//fetch query url
+
+fetch(query)
+
+//enable local search
+
+//save results to local storage
+
+//display saved results
+
+//display 5 day forecast
+
+//
